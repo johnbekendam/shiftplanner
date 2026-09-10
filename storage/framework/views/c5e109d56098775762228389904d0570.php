d80212f8c216389ef50b0a92a3f9bcda1eb3461f
John Bekendam <john@bekendam.net>
2026-09-10 23:08:49 +0200

    chore: stop tracking compiled Blade view cache

--- storage/framework/views/c5e109d56098775762228389904d0570.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<?php echo $bodyHtml; ?>
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('emails.layout', ['colors' => $colors], array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH /Users/jb/Development/Prodrive/shiftplanner/resources/views/emails/message.blade.php ENDPATH**/ ?>
